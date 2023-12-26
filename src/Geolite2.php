9d8d39a6c567d1b27f58a73cf5576c9341995c5d
artem.artemkin <artem.artemkin@topvisor.com>
2023-12-26 17:21:51 +0300

    add geolite2

---
+++ src/Geolite2.php
@@ -0,0 +1,31 @@
+<?php
+
+// https://github.com/topvisor/geolite2
+
+namespace Services\System_2;
+
+use Exception;
+use GeoIp2;
+use TV\TV;
+use Services\System_2;
+
+class Geolite2{
+
+	private static GeoIp2\Database\Reader $reader;
+
+	static function city(string $ip): ?GeoIp2\Model\City{
+		try{
+			return System_2\Geolite2::getReader()->city($ip);
+		}catch(Exception $e){
+			return null;
+		}
+	}
+
+	private static function getReader(): GeoIp2\Database\Reader{
+		if(!isset(System_2\Geolite2::$reader)) System_2\Geolite2::$reader = new GeoIp2\Database\Reader('./GeoLite2-City.mmdb');
+
+		return System_2\Geolite2::$reader;
+	}
+
+}
+
