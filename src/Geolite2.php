67ba1048fd1c45b9b9e21505c8da9b24e37d3117
artem.artemkin <artem.artemkin@topvisor.com>
2023-12-27 15:11:46 +0300

    fix semicolon

--- src/Geolite2.php
+++ src/Geolite2.php
@@ -11,16 +11,16 @@ class Geolite2{
 
 	static function city(string $ip): ?GeoIp2\Model\City{
 		try{
-			return Topvisor\Geolite2::getReader()->city($ip);
+			return Geolite2::getReader()->city($ip);
 		}catch(Exception $e){
 			return null;
 		}
 	}
 
 	private static function getReader(): GeoIp2\Database\Reader{
-		if(!isset(Topvisor\Geolite2::$reader)) Topvisor\Geolite2::$reader = new GeoIp2\Database\Reader(__DIR__ . '/GeoLite2-City.mmdb');
+		if(!isset(Geolite2::$reader)) Geolite2::$reader = new GeoIp2\Database\Reader(__DIR__ . '/GeoLite2-City.mmdb');
 
-		return Topvisor\Geolite2::$reader;
+		return Geolite2::$reader;
 	}
 
 }
