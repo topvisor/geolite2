41bf08d9c26e4e89197cb0462b1b5d9f7c9ed688
artem.artemkin <artem.artemkin@topvisor.com>
2023-12-27 15:05:24 +0300

    change namespace

--- src/Geolite2.php
+++ src/Geolite2.php
@@ -1,13 +1,9 @@
 <?php
 
-// https://github.com/topvisor/geolite2
-
-namespace Services\System_2;
+namespace Topvisor\Geolite2
 
 use Exception;
 use GeoIp2;
-use TV\TV;
-use Services\System_2;
 
 class Geolite2{
 
@@ -15,16 +11,16 @@ class Geolite2{
 
 	static function city(string $ip): ?GeoIp2\Model\City{
 		try{
-			return System_2\Geolite2::getReader()->city($ip);
+			return Topvisor\Geolite2::getReader()->city($ip);
 		}catch(Exception $e){
 			return null;
 		}
 	}
 
 	private static function getReader(): GeoIp2\Database\Reader{
-		if(!isset(System_2\Geolite2::$reader)) System_2\Geolite2::$reader = new GeoIp2\Database\Reader(__DIR__ . '/GeoLite2-City.mmdb');
+		if(!isset(Topvisor\Geolite2::$reader)) Topvisor\Geolite2::$reader = new GeoIp2\Database\Reader(__DIR__ . '/GeoLite2-City.mmdb');
 
-		return System_2\Geolite2::$reader;
+		return Topvisor\Geolite2::$reader;
 	}
 
 }
